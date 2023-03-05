f3d3914004ff73f2f498c4a4459bb9113c42cc91
Ítalo Mesquita <italombs22@gmail.com>
2023-03-05 16:12:41 +0000

    Chance namespace

--- console-chess/Program.cs
+++ console-chess/Program.cs
@@ -1,16 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+using board;
 namespace console_chess
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-
+            Board board = new Board(8, 8);
         }
     }
 }
