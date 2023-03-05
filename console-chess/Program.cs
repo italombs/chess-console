03cae6296ee951a81b36743a0858147dad6da095
Ítalo Mesquita <italombs22@gmail.com>
2023-03-05 18:14:33 +0000

    Change Screen class to improve the visualization of the board.

--- console-chess/Program.cs
+++ console-chess/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel;
 using board;
 using chessGame;
 using ChessGame;
@@ -13,14 +14,32 @@ namespace console_chess
             {
                 Board board = new Board(8, 8);
 
-                board.PutPiece(new King(Color.White, board), new Position(0, 1));
-                board.PutPiece(new Queen(Color.White, board), new Position(1, 1));
-                board.PutPiece(new Bishop(Color.White, board), new Position(3, 1));
-                board.PutPiece(new Pawn(Color.White, board), new Position(4, 4));
+                board.PutPiece(new Rook(Color.White, board), new ChessPosition('a', 1).ToPosition());
+                board.PutPiece(new Rook(Color.White, board), new ChessPosition('h', 1).ToPosition());
+                board.PutPiece(new Knight(Color.White, board), new ChessPosition('b', 1).ToPosition());
+                board.PutPiece(new Knight(Color.White, board), new ChessPosition('g', 1).ToPosition());
+                board.PutPiece(new Bishop(Color.White, board), new ChessPosition('c', 1).ToPosition());
+                board.PutPiece(new Bishop(Color.White, board), new ChessPosition('f', 1).ToPosition());
+                board.PutPiece(new Queen(Color.White, board), new ChessPosition('d', 1).ToPosition());
+                board.PutPiece(new King(Color.White, board), new ChessPosition('e', 1).ToPosition());
+
+                board.PutPiece(new Rook(Color.Black, board), new ChessPosition('a', 8).ToPosition());
+                board.PutPiece(new Rook(Color.Black, board), new ChessPosition('h', 8).ToPosition());
+                board.PutPiece(new Knight(Color.Black, board), new ChessPosition('b', 8).ToPosition());
+                board.PutPiece(new Knight(Color.Black, board), new ChessPosition('g', 8).ToPosition());
+                board.PutPiece(new Bishop(Color.Black, board), new ChessPosition('c', 8).ToPosition());
+                board.PutPiece(new Bishop(Color.Black, board), new ChessPosition('f', 8).ToPosition());
+                board.PutPiece(new Queen(Color.Black, board), new ChessPosition('d', 8).ToPosition());
+                board.PutPiece(new King(Color.Black, board), new ChessPosition('e', 8).ToPosition());
+
+                //board.PutPiece(new King(Color.White, board), new Position(0, 1));
+                //board.PutPiece(new Queen(Color.White, board), new Position(1, 1));
+                //board.PutPiece(new Bishop(Color.Black, board), new Position(3, 1));
+                //board.PutPiece(new Pawn(Color.Black, board), new Position(4, 4));
 
                 ChessPosition chessPosition = new ChessPosition('a', 8);
 
-                Console.WriteLine(chessPosition.ToPosition());
+                //Console.WriteLine(chessPosition.ToPosition());
 
                 Screen.PrintBoard(board);
             }
--- console-chess/Screen.cs
+++ console-chess/Screen.cs
@@ -7,17 +7,58 @@ namespace console_chess
     {
         public static void PrintBoard(Board board)
         {
+            ConsoleColor actualColor = Console.ForegroundColor;
+
             for (int r = 0; r < board.Rows; r++)
             {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Write(8 - r + " ");
+                Console.ForegroundColor = actualColor;
+
+
+
                 for (int c = 0; c < board.Columns; c++)
                 {
                     if (board.GetPiece(r, c) == null)
                         Console.Write("-" + " ");
                     else
-                        Console.Write(board.GetPiece(r, c) + " ");
+                        PrintPiece(board.GetPiece(r, c));
                 }
 
                 Console.WriteLine();
+            }
+
+
+
+            string cls = "a,b,c,d,e,f,g,h";
+            string[] columns = cls.Split(',');
+            Console.Write(" ");
+            Console.Write(" ");
+
+            Console.ForegroundColor = ConsoleColor.Red;
+
+            foreach (string column in columns)
+            {
+                Console.Write(column);
+                Console.Write(" ");
+            }
+
+            Console.WriteLine("");
+
+            Console.ForegroundColor = actualColor;
+        }
+
+        public static void PrintPiece(Piece piece)
+        {
+            if (piece.Color == Color.White)
+                Console.Write(piece + " ");
+            else if(piece.Color == Color.Black)
+            {
+                ConsoleColor actualColor = Console.ForegroundColor;
+
+                Console.ForegroundColor = ConsoleColor.DarkYellow;
+                Console.Write(piece + " ");
+                Console.ForegroundColor = actualColor;
             }
         }
     }
